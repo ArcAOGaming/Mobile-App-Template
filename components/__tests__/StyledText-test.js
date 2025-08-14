1d8bea67d0d9b0da345e99d170117e5debda35b4
Allan <31974198+BrewDogDev@users.noreply.github.com>
2025-08-14 18:13:10 -0400

    tests seperate

--- components/__tests__/StyledText-test.js
+++ components/__tests__/StyledText-test.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import { render } from '@testing-library/react-native';
+import { ThemeProvider } from '../../contexts/ThemeContext';
 
 import { MonoText } from '../StyledText';
 
 it('renders correctly', () => {
-  const { getByText } = render(<MonoText>Test text</MonoText>);
+  const { getByText } = render(
+    <ThemeProvider>
+      <MonoText>Test text</MonoText>
+    </ThemeProvider>
+  );
 
   const textElement = getByText('Test text');
   expect(textElement).toBeTruthy();
